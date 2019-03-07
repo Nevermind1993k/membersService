cb045233a6596e9d604492c8cb99d75236f68b09
Nevermind1993k <Nevermind1993k@gmail.com>
2019-03-07 13:44:12 +0200

    Added Spring Security

---
+++ src/main/java/com/rkovaliov/bu/configs/SpringSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.rkovaliov.bu.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationEntryPoint authEntryPoint;
+
+    @Autowired
+    public SpringSecurityConfig(AuthenticationEntryPoint authEntryPoint) {
+        this.authEntryPoint = authEntryPoint;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated();
+        http.httpBasic().authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        String password = "root";
+        String encryptedPassword = this.passwordEncoder().encode(password);
+
+        InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> mngConfig = auth.inMemoryAuthentication();
+        UserDetails u1 = User.withUsername("root").password(encryptedPassword).roles("USER").build();
+
+        mngConfig.withUser(u1);
+    }
+}
